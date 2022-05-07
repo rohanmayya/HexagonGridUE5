a2d665132058fbe3941d9b5416a0d8d0aa147d08
Rohan Mayya <rohan.mayya@gmail.com>
2022-05-07 11:52:42 +0530

    initial commit

---
+++ Source/HexagonGrid/HexagonGridGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HexagonGridGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEXAGONGRID_API AHexagonGridGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
